6280f2ea4e504ecd33f77a32c2c86296d0733867
Anil Madhavapeddy <anil@recoil.org>
2016-07-18 10:56:42 +0100

    cosmetic, remove a redundant unit

--- src-bin/jsontee_bin.ml
+++ src-bin/jsontee_bin.ml
@@ -59,8 +59,7 @@ let run cmd () =
 let setup_log style_renderer level =
   Fmt_tty.setup_std_outputs ?style_renderer ();
   Logs.set_level level;
-  Logs.set_reporter (Logs_fmt.reporter ());
-  ()
+  Logs.set_reporter (Logs_fmt.reporter ())
 
 open Cmdliner
 
